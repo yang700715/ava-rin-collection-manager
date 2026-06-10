d769e54ed04bec1af62a14e27bf3f37f27e479c6
小小源 <yang700715@gmail.com>
2026-06-10 12:40:20 +0800

    Update Ava Rin collection

--- src/data/seedData.js
+++ src/data/seedData.js
@@ -14,9 +14,333 @@ const seedData = {
       "name": "Ava_凜 工作篇 1",
       "slug": "ava-rin-work-1",
       "description": "工作情境貼圖，包含加班中、開工啦、收到、馬上處理、會議中、下班啦。"
+    },
+    {
+      "id": "series-mq7kuk01-duuife",
+      "categoryId": "cat-line",
+      "name": "Ava_凜 日常生活篇 1",
+      "slug": "Ava_凜-日常生活篇-1",
+      "description": ""
+    },
+    {
+      "id": "series-mq7kwutu-ihsl9j",
+      "categoryId": "cat-line",
+      "name": "Ava_凜 直播中篇 1",
+      "slug": "Ava_凜-直播中篇-1",
+      "description": ""
     }
   ],
   "items": [
+    {
+      "id": "item-mq7kxhtw-mss5dh",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "01 開播中 白T耳機麥克風",
+      "displayText": "01 開播中 白T耳機麥克風",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/01-開播中-白T耳機麥克風.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhtx-o9lra6",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "02 直播中 白襯衫麥克風",
+      "displayText": "02 直播中 白襯衫麥克風",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/02-直播中-白襯衫麥克風.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhtz-cstvh3",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "03 直播中 白襯衫耳機水瓶",
+      "displayText": "03 直播中 白襯衫耳機水瓶",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/03-直播中-白襯衫耳機水瓶.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhu0-pokjbl",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "04 直播中 黑T耳機麥克風",
+      "displayText": "04 直播中 黑T耳機麥克風",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/04-直播中-黑T耳機麥克風.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhu1-2j95dz",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "05 直播中 白T生菜桌面",
+      "displayText": "05 直播中 白T生菜桌面",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/05-直播中-白T生菜桌面.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhu2-xa976h",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "06 欸 白襯衫摸頭疑惑",
+      "displayText": "06 欸 白襯衫摸頭疑惑",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/06-欸-白襯衫摸頭疑惑.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhu3-1jgkux",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "07 思考中 米色外套麥克風",
+      "displayText": "07 思考中 米色外套麥克風",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/07-思考中-米色外套麥克風.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhu4-n23ifj",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "08 黑帽T麥克風",
+      "displayText": "08 黑帽T麥克風",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/08-黑帽T麥克風.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhu5-tvv8q3",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "09 紅T側臉Q版",
+      "displayText": "09 紅T側臉Q版",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/09-紅T側臉Q版.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhu7-4hh84a",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "10 紅T側臉Q版桌面",
+      "displayText": "10 紅T側臉Q版桌面",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/10-紅T側臉Q版桌面.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhu8-nle4gr",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "11 紅T嘟嘴 Q版桌面",
+      "displayText": "11 紅T嘟嘴 Q版桌面",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/11-紅T嘟嘴-Q版桌面.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhu9-akl7wp",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "12 戶外白T撥頭髮",
+      "displayText": "12 戶外白T撥頭髮",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/12-戶外白T撥頭髮.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhua-nahhhu",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "13 白T拿水瓶",
+      "displayText": "13 白T拿水瓶",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/13-白T拿水瓶.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kxhub-vqbb5x",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kwutu-ihsl9j",
+      "title": "14 粉色上衣拿水瓶",
+      "displayText": "14 粉色上衣拿水瓶",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-直播中篇-1/14-粉色上衣拿水瓶.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvlz-g0x2r0",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "01 Ava 凜 日常生活篇 1",
+      "displayText": "01 Ava 凜 日常生活篇 1",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/01-Ava-凜-日常生活篇-1.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvlz-8kt4mr",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "02 哈囉",
+      "displayText": "02 哈囉",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/02-哈囉.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm0-gvp1iz",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "03 讚啦",
+      "displayText": "03 讚啦",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/03-讚啦.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm1-psvxkj",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "04 謝謝",
+      "displayText": "04 謝謝",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/04-謝謝.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm2-wgrau8",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "05 晚安",
+      "displayText": "05 晚安",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/05-晚安.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm2-daxooo",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "06 好害羞",
+      "displayText": "06 好害羞",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/06-好害羞.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm3-9fglzv",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "07 加油",
+      "displayText": "07 加油",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/07-加油.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm4-168pka",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "08 收到",
+      "displayText": "08 收到",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/08-收到.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm4-wbpjka",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "09 愛你喔",
+      "displayText": "09 愛你喔",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/09-愛你喔.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm5-85o113",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "10 等一下",
+      "displayText": "10 等一下",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/10-等一下.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm5-utgbq1",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "11 抱歉",
+      "displayText": "11 抱歉",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/11-抱歉.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm6-gfhdwk",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "12 生氣氣",
+      "displayText": "12 生氣氣",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/12-生氣氣.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm7-fd8l8i",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "13 無語",
+      "displayText": "13 無語",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/13-無語.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm7-2b5bqw",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "14 哈哈哈",
+      "displayText": "14 哈哈哈",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/14-哈哈哈.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm8-kski6w",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "15 辛苦了",
+      "displayText": "15 辛苦了",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/15-辛苦了.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm8-ykeive",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "16 沒問題",
+      "displayText": "16 沒問題",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/16-沒問題.png",
+      "notes": ""
+    },
+    {
+      "id": "item-mq7kvvm9-78w6mf",
+      "categoryId": "cat-line",
+      "seriesId": "series-mq7kuk01-duuife",
+      "title": "17 哇",
+      "displayText": "17 哇",
+      "status": "完成",
+      "image": "/images/line-stickers/Ava_凜-日常生活篇-1/17-哇.png",
+      "notes": ""
+    },
     {
       "id": "ava-rin-work-1-001",
       "categoryId": "cat-line",
